54eccea551d04a7e86b1da85efbd2140e53b1575
Rob Campbell <git@raacampbell.com>
2020-03-27 09:59:17 +0000

    Add function to make a summary plot of a results file

---
+++ code/+boundingBoxesFromLastSection/+plotting/plotResultsFile.m
@@ -0,0 +1,109 @@
+function plotResultsFile(fname)
+% Plot data from a single results file
+%
+%  function boundingBoxesFromLastSection.test.plotResultsFile(fname)
+%
+% Purpose
+% This function helps to highlight which samples still need more work
+%
+% Inputs
+% fname - path to results file, which will correspond to the "initial" data
+%
+%
+% Outputs
+% none
+%
+%
+% Rob Campbell - SWC 2020
+
+
+resStruct = boundingBoxesFromLastSection.test.resultsFileToStruct(fname);
+
+if isempty(resStruct)
+    return
+end
+
+
+
+f = fields(resStruct);
+
+sqmm_sum = zeros(length(f),1);
+sqmm_max = zeros(length(f),1);
+extra_sum = zeros(length(f),1);
+extra_max = zeros(length(f),1);
+
+for ii=1:length(f)
+
+    tF = resStruct.(f{ii});
+
+    fprintf('%d. %s\n', ii, f{ii})
+
+    % Log info for plotting
+    sqmm_sum(ii) = sum(tF.sqmmMissed);
+    sqmm_max(ii) = max(tF.sqmmMissed);
+    extra_sum(ii) = sum(tF.sqmmExtra);
+    extra_max(ii) = max(tF.sqmmExtra);
+
+end
+
+
+
+clf
+
+subplot(3,2,1)
+plot(sqmm_sum, '.r-')
+hold on 
+plot(xlim,[0,0],'k:')
+grid on
+hold off
+xlabel('Acquisition #')
+ylabel('Square mm missed')
+title('Total square mm missed (lower better)')
+
+subplot(3,2,2)
+plot(sqmm_max, '.r-')
+hold on 
+plot(xlim,[0,0],'k:')
+grid on
+hold off
+xlabel('Acquisition #')
+ylabel('Aquare mm missed')
+title('Worst section square mm missed (lower better)')
+
+subplot(3,2,3)
+plot(extra_sum, '.r-')
+hold on 
+plot(xlim,[0,0],'k:')
+grid on
+hold off
+xlabel('Acquisition #')
+ylabel('Square mm extra')
+title('Total square mm extra (lower better)')
+
+subplot(3,2,4)
+plot(extra_max, '.r-')
+hold on 
+plot(xlim,[0,0],'k:')
+grid on
+hold off
+xlabel('Acquisition #')
+ylabel('Aquare mm extra')
+title('Section with most extra square mm (lower better)')
+
+
+subplot(3,2,5)
+plot(extra_sum,sqmm_sum,'.r')
+hold on
+xoffset = diff(xlim)*0.0075;
+yoffset = diff(ylim)*0.02;
+for ii=1:length(extra_sum)
+    text(extra_sum(ii)+xoffset, sqmm_sum(ii)+yoffset,num2str(ii))
+end
+hold off
+xlabel('Extra sq mm')
+ylabel('Missed sq mm')
+
+subplot(3,2,6)
+hist(sqmm_sum,25)
+xlabel('Missed sq mm')
+ylabel('# acquisitions')
