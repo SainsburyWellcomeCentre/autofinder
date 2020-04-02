85c2e78bc888c5c6e22d7143648ac7f697e1234d
Rob Campbell <git@raacampbell.com>
2020-04-02 16:29:12 +0100

    Simple function to remove the smallest border from a section

---
+++ code/+boundingBoxesFromLastSection/+groundTruth/removeSmallestBorder.m
@@ -0,0 +1,48 @@
+function pStack = removeSmallestBorder(pStack,inds)
+% Remove the smallest sample border from pStack.borders{inds}
+%
+% pStack = boundingBoxesFromLastSection.groundTruth.stackToGroundTruth(removeSmallestBorder(pStack,inds)
+%
+% Purpose
+% Used to help curate data. Removes whichever is the smallest border in each cell defined by 
+% the vector inds.
+%
+% 
+% Inputs
+% pStack - pStack data structure.
+% inds - a vector to loop over. These are indecies of pStack.borders
+%            have multiple samples. 
+%
+% Outputs
+% pStack - pStack data structure.
+%
+%
+
+if ~isstruct(pStack)
+    fprintf('pStack should be a structure\n')
+    return
+end
+
+if ~isfield(pStack,'borders')
+    fprintf('No borders field in pStruct. First you need to run boundingBoxesFromLastSection.groundTruth.genGroundTruthBorders\n')
+    return
+end
+
+
+for ii=1:length(inds)
+    tInd = inds(ii);
+
+    if tInd > length(pStack.borders{1})
+        fprintf('index %d is out of range. length borders is %d. Skipping.\n', ...
+            tInd, length(pStack.borders{1}))
+        continue
+    end
+
+    %Find smallest border and remove it
+    tB = pStack.borders{1}{tInd};
+    L = cellfun(@length, tB);
+    [~,minInd] = min(L);
+    tB(minInd)=[];
+
+    pStack.borders{1}{tInd} = tB;
+end
